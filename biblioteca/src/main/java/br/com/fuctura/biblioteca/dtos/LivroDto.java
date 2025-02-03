efe5f0e92a415348ec90b8230d50bf68cb75e79f
Gabri <gabrielmg.silva@outlook.com>
2025-02-03 15:47:36 -0300

    01-02-25 - configuração e implementação da classes LivroController; LivroDto; LivroService entre outros.

---
+++ biblioteca/src/main/java/br/com/fuctura/biblioteca/dtos/LivroDto.java
@@ -0,0 +1,73 @@
+package br.com.fuctura.biblioteca.dtos;
+
+import br.com.fuctura.biblioteca.enums.Tamanho;
+
+public class LivroDto {
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private String texto;
+
+    private CategoriaDto categoria;
+    private Tamanho tamanho;
+
+    public LivroDto(Integer id, String titulo, String autor, String texto, CategoriaDto categoria, Tamanho tamanho) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.texto = texto;
+        this.categoria = categoria;
+        this.tamanho = tamanho;
+    }
+
+    public LivroDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public CategoriaDto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaDto categoria) {
+        this.categoria = categoria;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
+}
